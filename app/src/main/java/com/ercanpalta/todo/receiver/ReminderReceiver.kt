4eea49977a7176ce0ac4a631f96eca3bae9316ed
Ercan Palta <ercan.palta@outlook.com>
2022-09-22 20:27:41 +0300

    Notification implemented

--- app/src/main/java/com/ercanpalta/todo/receiver/ReminderReceiver.kt
+++ app/src/main/java/com/ercanpalta/todo/receiver/ReminderReceiver.kt
@@ -1,12 +1,38 @@
 package com.ercanpalta.todo.receiver
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.ercanpalta.todo.R
 
 class ReminderReceiver: BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
-        Toast.makeText(p0,"receiver",Toast.LENGTH_LONG).show()
+
+        val notificationManager = p0?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel("com.ercanpalta.todo.receiver", "Reminder",
+                NotificationManager.IMPORTANCE_HIGH)
+            channel.apply {
+                enableVibration(true)
+                enableLights(true)
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(p0,"com.ercanpalta.todo.receiver")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Title")
+            .setContentText("Content text")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setDefaults(Notification.DEFAULT_ALL)
+
+
+        notificationManager.notify(0, builder.build())
     }
 }
