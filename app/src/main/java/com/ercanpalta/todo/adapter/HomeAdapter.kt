1c96eb40afcb90f46bd9660a3f924a4e8c3d7052
Ercan Palta <ercan.palta@outlook.com>
2022-10-18 11:33:56 +0300

    Tracker counter made invisible when row item not expanded

--- app/src/main/java/com/ercanpalta/todo/adapter/HomeAdapter.kt
+++ app/src/main/java/com/ercanpalta/todo/adapter/HomeAdapter.kt
@@ -143,6 +143,7 @@ class HomeAdapter (private val dataSet: ArrayList<ToDo>, val fragment: HomeFragm
                 val menu = binding.longclickMenu
                 val dateContainer = binding.reminderDateContainer
                 val addFireIcon = binding.addFireIcon
+                val trackerCounter= binding.trackerCounterText
                 val trackerMax = binding.trackerMaxText
 
                 if(menu.visibility == View.GONE){
@@ -152,12 +153,14 @@ class HomeAdapter (private val dataSet: ArrayList<ToDo>, val fragment: HomeFragm
                         detail.visibility = View.VISIBLE
                         dateContainer.visibility = View.VISIBLE
                         addFireIcon.visibility = View.VISIBLE
+                        trackerCounter.visibility = View.VISIBLE
                         trackerMax.visibility = View.VISIBLE
 
                     }else{
                         detail.visibility = View.GONE
                         dateContainer.visibility = View.GONE
                         addFireIcon.visibility = View.GONE
+                        trackerCounter.visibility = View.GONE
                         trackerMax.visibility = View.GONE
                     }
                 }
